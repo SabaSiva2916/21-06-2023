ddec2f4fbe649325f313c9893ccf0022a83ab98e
SabaSiva2916 <sabasiva2916@gmail.com>
2023-06-22 20:04:25 +0530

    Dev works

--- src/org/browser/Test.java
+++ src/org/browser/Test.java
@@ -13,26 +13,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Test {
-   static WebDriver driver;
+    static WebDriver driver;
+
     public static void main(String[] args) throws InterruptedException, IOException {
 
-	 driver = new ChromeDriver();
+	driver = new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.get("https://www.facebook.com/login/");
 	driver.findElement(By.id("email")).sendKeys("aji");
 	driver.findElement(By.id("pass")).sendKeys("1234");
 	driver.findElement(By.name("login")).click();
-	TakesScreenshot ts=(TakesScreenshot)driver;
+	TakesScreenshot ts = (TakesScreenshot) driver;
 	File screenshotAs = ts.getScreenshotAs(OutputType.FILE);
-	File f=new File("C:\\Users\\sabas\\OneDrive\\Desktop\\New folder (4)\\login.png");
-	//copyfile-static method
-	FileUtils.copyFile(screenshotAs,f);
-	
-	
-	
-	
-	
-	
+	File f = new File("C:\\Users\\sabas\\OneDrive\\Desktop\\New folder (4)\\login.png");
+	// copyfile-static method
+	FileUtils.copyFile(screenshotAs, f);
+	driver.findElement(By.xpath("//button[@text='create')")).click();
+	Thread.sleep(3000);
+	driver.findElement(By.name("firstname")).sendKeys("Welcome");
+	driver.findElement(By.name("lastname")).sendKeys("GreensOMR");
+
     }
 
 }
