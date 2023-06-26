4f7af38b5df19dc303bb2e9d71f5762e11f2bbb0
SabaSiva2916 <sabasiva2916@gmail.com>
2023-06-26 19:58:09 +0530

    test works

--- src/org/browser/Test.java
+++ src/org/browser/Test.java
@@ -2,6 +2,8 @@ package org.browser;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -13,26 +15,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Test {
-   static WebDriver driver;
+    static WebDriver driver;
+
     public static void main(String[] args) throws InterruptedException, IOException {
 
-	 driver = new ChromeDriver();
+	driver = new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.get("https://www.facebook.com/login/");
 	driver.findElement(By.id("email")).sendKeys("aji");
 	driver.findElement(By.id("pass")).sendKeys("1234");
 	driver.findElement(By.name("login")).click();
-	TakesScreenshot ts=(TakesScreenshot)driver;
+	TakesScreenshot ts = (TakesScreenshot) driver;
 	File screenshotAs = ts.getScreenshotAs(OutputType.FILE);
-	File f=new File("C:\\Users\\sabas\\OneDrive\\Desktop\\New folder (4)\\login.png");
-	//copyfile-static method
-	FileUtils.copyFile(screenshotAs,f);
-	
-	
-	
-	
-	
-	
+	File f = new File("C:\\Users\\sabas\\OneDrive\\Desktop\\New folder (4)\\login.png");
+	// copyfile-static method
+	FileUtils.copyFile(screenshotAs, f);
+	String str = "Java Programm";
+	String output = "";
+	for (int i = str.length() - 1; i >= 0; i--) {
+	    char c = str.charAt(i);
+	    output = output + c;
+	}
+	System.out.println(output);
+	Map<Character,Integer> map = new LinkedHashMap<>();
+	for(int i=0; i<str.length();i++) {
+	    char c= str.charAt(i);
+	    if (map.containsKey(c)) {
+		int value = map.get(c);
+		map.put(c, value+1);
+	    }else {
+		map.put(c, 1);
+	    }
+	}
+	System.out.println(map);
+
     }
 
 }
